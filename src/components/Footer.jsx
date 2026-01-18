8ad0c77de3af9e1d4a42152c2261d9374355d427
DavideCotigliani <davide.cotigliani@gmail.com>
2026-01-18 17:21:06 +0100

    [UPD] FIxing the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import { Col, Container, Row, Form, FormSelect } from 'react-bootstrap';
 const Footer = () => {
   return (
-    <div>
-      <h2>Componente secondario</h2>
+      <div>
+          <footer className='footer mt-3 pt-3'>
+              <Container fluid className='px-5'>
+                  <Row className="mb-4">
+                       <Col md={4} className="mb-3">
+            <p>
+              <h3>Scopri l'umore di oggi.</h3>
+            </p>
+            <p>Powered by Davide Cotigliani</p>
+          </Col>
+                  </Row>
+              </Container>
+              
+          </footer>
     </div>
   )
 }
